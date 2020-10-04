1801d7d260c9817d75740c67a3965c2cce7b0354
Gonzalo Cercos Vazquez <gcercos@altech.es>
2020-10-04 11:53:02 +0200

    Feat: Adding the header component

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -8,7 +8,7 @@ export const Container = styled.div`
 export const Item = styled.div`
     color: white;
     margin-bottom: 10px;
-    max-width: 670px;
+    max-width: 700px;
     width: 100%;
 
     &:first-of-type {
