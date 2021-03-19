8a398f75238fea99a5ca4feab825c5d8eeafcdff
Gonzalo Cercos <goncholo@gmail.com>
2021-03-19 15:07:39 +0100

    Feat: Footer

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div `
+    display: flex;
+    padding: 70px 56px;
+    margin: auto;
+    max-width: 1000px;
+    flex-direction: column;
+
+    @media (max-width: 1000px) {
+        padding: 70px 30px;
+    }
+`;
+
+export const Row = styled
+export const Column = styled.div `
+    display: flex;
+    
+`;
+export const Link = styled
+export const Title = styled
+export const Text = styled
+export const Break = styled
